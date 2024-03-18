761f759651053b20e5573033ad51176e05a11a8c
Jola9064 <john-christopher.laske@tu-ilmenau.de>
2024-03-18 16:41:23 +0100

    uploaded
    uploaded

---
+++ GetYoutubePalyList/GetYoutubePalyList/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace GetYoutubePalyList
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            videoTitles = new DataGridViewTextBoxColumn();
+            videoURLs = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button1.Location = new Point(12, 407);
+            button1.Name = "button1";
+            button1.Size = new Size(776, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Get Playlist Infos";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 381);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Playlist URL:";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(89, 378);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(699, 23);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "https://www.youtube.com/playlist?list=PLlVlyGVtvuVlklX5bYqk9RHnOSmUP6j0h";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { videoTitles, videoURLs });
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(776, 360);
+            dataGridView1.TabIndex = 4;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // videoTitles
+            // 
+            videoTitles.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            videoTitles.HeaderText = "Video Titles";
+            videoTitles.Name = "videoTitles";
+            videoTitles.Width = 93;
+            // 
+            // videoURLs
+            // 
+            videoURLs.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            videoURLs.HeaderText = "Video URLs";
+            videoURLs.Name = "videoURLs";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn videoTitles;
+        private DataGridViewTextBoxColumn videoURLs;
+    }
+}
---
+++ GetYoutubePalyList/GetYoutubePalyList/Form1.cs
@@ -0,0 +1,86 @@
+using System.Data;
+using System.Security.Policy;
+using System.Windows.Forms;
+
+namespace GetYoutubePalyList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            VideoInfo[] videoinfos = await GetDataFromWebsite(textBox1.Text);
+
+            foreach (VideoInfo vinfo in videoinfos)
+            {
+                dataGridView1.Rows.Add(vinfo.videoName, vinfo.videoURL);
+            }
+        }
+
+        internal async Task<VideoInfo[]> GetDataFromWebsite(string url)
+        {
+            string startSearch = "else {window.addEventListener('script-load-dpj',";
+            string endSearch = "</div>";
+            string beforeEachEntry = "\"ADD_TO_QUEUE_TAIL\"";
+            string beforeurl = "{\"webCommandMetadata\":{\"url\":\"/watch";
+            string afterUrl = "\",\"webPageType\"";
+            string filterTitleBetter = ",\"width\":336,\"height\":188}]}";
+            string beforeTitle = ",\"title\":{\"runs\":[{\"text\":\"";
+            string aftertitle = "\"";
+
+            VideoInfo[] videoInfos;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string xml = await response.Content.ReadAsStringAsync();
+                    string[] allData = xml.Split(startSearch)[1].Split(endSearch)[0].Split(beforeEachEntry);
+
+                    videoInfos = new VideoInfo[allData.Length - 1];
+
+                    for (int i = 1; i < allData.Length; i++)
+                    {
+                        if (i == 11)
+                        {
+                            int justforme = 1;
+                        }
+
+                        string[] splitForVideoName1 = allData[i].Split(filterTitleBetter);
+                        string[] splitForVideoName = splitForVideoName1[splitForVideoName1.Length - 1].Split(beforeTitle);
+                        string videoName = splitForVideoName[1].Split(aftertitle)[0];
+                        string videoURL = "https://www.youtube.com/watch" + allData[i].Split(beforeurl)[1].Split(afterUrl)[0];
+
+
+
+                        videoInfos[i - 1] = new VideoInfo() { videoURL = videoURL, videoName = videoName };
+                    }
+                    client.Dispose();
+                    return videoInfos;
+                }
+                else
+                {
+                    client.Dispose();
+                    throw new Exception();
+                }
+            }
+        }
+
+        internal class VideoInfo
+        {
+            internal string videoURL;
+            internal string videoName;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
